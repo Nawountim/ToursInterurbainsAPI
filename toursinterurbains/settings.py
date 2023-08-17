70209d3b44e4e6408fe5791286d80adbde6774a9
Nawountim <arnaudgbati22@gmail.com>
2023-08-17 11:44:33 +0000

    Modification des fonctions ussd

--- toursinterurbains/settings.py
+++ toursinterurbains/settings.py
@@ -160,3 +160,10 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+
+
+""" EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool) """
